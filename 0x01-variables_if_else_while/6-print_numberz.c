1dd46fb167c7501c969a2222e5bc42e0b6e4aa2e
daniel-nil <danoyindamola94@gmail.com>
2023-02-17 13:22:19 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - prints all single digit numbers pf base 10 starting from 0
+ * Return: Always 0 (success0
+ */
+int main(void)
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
